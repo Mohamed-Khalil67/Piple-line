308b4799623db8b565a708710452b78b24046e76
Mohamed Khalil <71330579+Mohamed-Khalil67@users.noreply.github.com>
2021-01-04 12:39:21 +0100

    Add files via upload

---
+++ ml/model.py
@@ -0,0 +1,10 @@
+import joblib
+import numpy as np
+from datetime import datetime
+from utils.utils import *
+
+def DataManager(d:DataHandler=None, fr: FeatureRecipe=None, fe:FeatureExtractor=None): # script model.py qui sort une model .joblib , alors dans model.py datamanager model builder et va sortir un fichier .joblib
+    """
+        Fonction qui lie les 3 premières classes de la pipeline et qui return FeatureExtractor.split(0.1)
+    """
+    pass
